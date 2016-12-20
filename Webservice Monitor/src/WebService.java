5ea46a136e4c6afdaf9f7dc6d5f109b522172dc5
evilcatfish <cajunwednesday@gmail.com>
2016-12-20 17:48:52 +0000

    tiddly bits

--- Webservice Monitor/src/WebService.java
+++ Webservice Monitor/src/WebService.java
@@ -1,38 +1,12 @@
 
 public class WebService {
-	private String wsdl;
-	private String name;
-	private String request;
-	private String expectedResponse;
+	String wsdl;
+	String name;
+	String request;
+	String expectedResponse;
 	
 	
-	public String getWsdl() {
-		return wsdl;
-	}
-	public void setWsdl(String wsdl) {
-		this.wsdl = wsdl;
-	}
 	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getRequest() {
-		return request;
-	}
-	public void setRequest(String request) {
-		this.request = request;
-	}
-
-	public String getExpectedResponse() {
-		return expectedResponse;
-	}
-	public void setExpectedResponse(String expectedResponse) {
-		this.expectedResponse = expectedResponse;
-	}
 	
 
 }
